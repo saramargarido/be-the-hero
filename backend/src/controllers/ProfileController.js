512800fd201d5ce0d3534fb8f69370088f0914a3
saramargarido <saramargarido@gmail.com>
2020-03-28 10:43:31 -0300

    correção bug profile

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -2,7 +2,7 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index (request, response) {
-        const ong_id = request.header.authorization;
+        const ong_id = request.headers.authorization;
 
         const incidents = await connection('incidents')
         .where('ong_id', ong_id)
